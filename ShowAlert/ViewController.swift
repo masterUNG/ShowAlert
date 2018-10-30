61085c5b7c93748a6076f7e4b1eb1af2f9de0477
masterUNG <phrombutr@gmail.com>
2018-10-30 08:21:06 +0700

    update 30oct

--- ShowAlert/ViewController.swift
+++ ShowAlert/ViewController.swift
@@ -14,6 +14,28 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        showAlert(title: "Hello User", message: "Please Choose OK/NO ?")
+    }
+    
+    func showAlert(title: String, message: String) -> Void {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        
+//       Create Button OK
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+            print("Choose OK")
+        }))
+        
+//        Create Button NO
+        alert.addAction(UIAlertAction(title: "NO", style: UIAlertAction.Style.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+            print("Choose NO")
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 
 
 }
